fa3107938a694e346b94219f279799b06faa2cc8
Ricardo Luís Vaz Silva <ricolvs123@gmail.com>
2023-04-20 22:06:55 -0300

    major A_ChangeModel cleanup

--- src/playsim/p_mobj.cpp
+++ src/playsim/p_mobj.cpp
@@ -1367,6 +1367,15 @@ void DActorModelData::Serialize(FSerializer& arc)
 		("hasModel", hasModel);
 }
 
+void DActorModelData::OnDestroy()
+{
+	modelIDs.Reset();
+	modelFrameGenerators.Reset();
+	skinIDs.Reset();
+	surfaceSkinIDs.Reset();
+	animationIDs.Reset();
+}
+
 //----------------------------------------------------------------------------
 //
 // PROC P_ExplodeMissile
